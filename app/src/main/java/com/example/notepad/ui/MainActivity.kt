6c40393b14fc55dcaeed6997f7cf86bb89e3d66e
msfmsfmsf <1017629235@qq.com>
2022-05-29 21:41:34 +0800

    编写主界面RecyclerView显示相关

--- app/src/main/java/com/example/notepad/ui/MainActivity.kt
+++ app/src/main/java/com/example/notepad/ui/MainActivity.kt
@@ -1,23 +1,27 @@
 package com.example.notepad.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.Window
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.notepad.R
 import com.example.notepad.dao.DBService
+import com.example.notepad.pojo.Affairs
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private val todoList=LinkedList<Int>()
+    private val todoList=LinkedList<Affairs>()
     override fun onCreate(savedInstanceState: Bundle?) {
         //隐藏ActionBar
         val actionBar: ActionBar? = supportActionBar
@@ -33,31 +37,35 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager=layoutManager
         recyclerView.adapter=adapter
 
-        val fab=findViewById<FloatingActionButton>(R.id.fab)
+        //增添记录的悬浮按钮，为其设置事件
+        val fab=findViewById<FloatingActionButton>(R.id.fabAdd)
         fab.setOnClickListener {
             val intent= Intent(this,EditActivity::class.java)
             startActivity(intent)
         }
+
+        //设置多选删除事件
+        findViewById<Button>(R.id.mainMultiSelect).setOnClickListener {
+            adapter.ischecked=!adapter.ischecked
+            adapter.notifyDataSetChanged()
+        }
     }
 
     private fun initTitle() {
-        todoList.add(1)
-        todoList.add(2)
-        todoList.add(3)
-        todoList.add(32)
-        todoList.add(333)
-        todoList.add(3344)
+        todoList.clear()
+        todoList.addAll(DBService.getTodoList(this, true))
+        todoList.addAll(DBService.getTodoList(this, false))
+
     }
 }
-class TodoListAdapter(private val todoList:List<Int>): RecyclerView.Adapter<TodoListAdapter.ViewHolder>(){
+class TodoListAdapter(private val todoList:List<Affairs>): RecyclerView.Adapter<TodoListAdapter.ViewHolder>(){
+    var ischecked=false
     inner class ViewHolder(view: View):RecyclerView.ViewHolder(view){
         val title:TextView=view.findViewById(R.id.title)
-        val text:TextView=view.findViewById(R.id.text)
-    }
-    class obs(): RecyclerView.AdapterDataObserver(){
-        override fun onChanged() {
-            super.onChanged()
-        }
+        val checkBox: CheckBox =view.findViewById(R.id.checkbox)
+        val item: ConstraintLayout =view.findViewById(R.id.item)
+        val updateTime: TextView =view.findViewById(R.id.updateTime)
+        val createTime: TextView =view.findViewById(R.id.createTime)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view=LayoutInflater.from(parent.context).inflate(R.layout.todolist_item,parent,false)
@@ -65,7 +73,19 @@ class TodoListAdapter(private val todoList:List<Int>): RecyclerView.Adapter<Todo
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.title.text=todoList[position].toString()
+        if(ischecked) holder.checkBox.visibility=View.VISIBLE
+        else holder.checkBox.visibility=View.GONE
+        holder.item.background.alpha= 150
+        if(position%2==1) holder.item.setBackgroundResource(R.drawable.blue)
+        holder.title.text=todoList[position].title
+        holder.updateTime.text=timeStampToTime(todoList[position].updateTime)
+        holder.createTime.text=timeStampToTime(todoList[position].createTime)
+    }
+
+    //将数据库中的时间戳转换为显示的时间
+    private fun timeStampToTime(t:Int):String{
+        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        return sdf.format(Date(java.lang.String.valueOf(t).toLong()*1000)) // 时间戳转换成时间
     }
 
     override fun getItemCount(): Int {
